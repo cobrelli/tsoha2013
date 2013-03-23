f04119ec32ebd0bff8a1b3d50bc6fbff247630f4
cobrelli <cobrelli@gmail.com>
2013-03-23 14:27:42 +0200

    varaus sivua

--- PalveluBisnes/varaus.php
+++ PalveluBisnes/varaus.php
@@ -10,51 +10,55 @@ include 'sivunYlaOsa.php';
     <div id="varaus_vasen">
         <h3>Tee uusi varaus</h3>
 
-        <fieldset>
-            <p><label class="kyltti" for="etunimi">Etunimi: </label>
-                <input type="text" name="Etunimi" class="kentta">
-            </p>
-            <p><label class="kyltti" for="Sukunimi">Sukunimi: </label>
-                <input type="text" name="Sukunimi" class="kentta">
-            </p>
-            <p><label class="kyltti" for="asiakaspalvelija">Asiakaspalvelija: </label></p>
-            <select>
-                <option value="1">palvelija 1</options>
-                <option value="2">palvelija 2</options>
-                <option value="3">palvelija 3</options>
-            </select>
-            <p><label class="kyltti" for="toimipiste">Toimipiste: </label></p>
-            <select>
-                <option value="1">paikka 1</options>
-                <option value="2">paikka 2</options>
-                <option value="3">paikka 3</options>
-            </select>
+        <form>
+            <fieldset>
+                <p><label class="kyltti" for="etunimi">Etunimi: </label>
+                    <input type="text" name="Etunimi" class="kentta">
+                </p>
+                <p><label class="kyltti" for="Sukunimi">Sukunimi: </label>
+                    <input type="text" name="Sukunimi" class="kentta">
+                </p>
+                <p><label class="kyltti" for="asiakaspalvelija">Asiakaspalvelija: </label></p>
+                <select>
+                    <option value="1">palvelija 1</options>
+                    <option value="2">palvelija 2</options>
+                    <option value="3">palvelija 3</options>
+                </select>
+                <p><label class="kyltti" for="toimipiste">Toimipiste: </label></p>
+                <select>
+                    <option value="1">paikka 1</options>
+                    <option value="2">paikka 2</options>
+                    <option value="3">paikka 3</options>
+                </select>
 
-            <p><label class="kyltti" for="palvelu">Palvelu: </label></p> 
-            <select>
-                <option value="1">palvelu 1</options>
-                <option value="2">palvelu 2</options>
-                <option value="3">palvelu 3</options>
-            </select>
+                <p><label class="kyltti" for="palvelu">Palvelu: </label></p> 
+                <select>
+                    <option value="1">palvelu 1</options>
+                    <option value="2">palvelu 2</options>
+                    <option value="3">palvelu 3</options>
+                </select>
 
-            <p><label class="kyltti" for="pvm">Päivämäärä: </label>
-                <input type="date" name="pvm">
-            </p>
-            klo
-            <select>
-                <option value="8">8.00</options>
-                <option value="9">9.00</options>
-            </select>
+                <p><label class="kyltti" for="pvm">Päivämäärä: </label>
+                    <input type="date" name="pvm">
+                </p>
+                klo
+                <select>
+                    <option value="8">8.00</options>
+                    <option value="9">9.00</options>
+                </select>
 
-        </fieldset>
-        <input type="submit" value="Lähetä">
+            </fieldset>
+            <input type="submit" value="Lähetä">
+        </form>
     </div>
 
     <div id="varaus_oikea">
         <h3>Peru varauksesi</h3>
 
-        Varausnumero: <input type="text" name="varausnumero"><br>
-        <input type="submit" value="Peru">
+        <form>
+            Varausnumero: <input type="text" name="varausnumero"><br>
+            <input type="submit" value="Peru">
+        </form>
     </div>
 
 </article>
