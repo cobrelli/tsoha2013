6ded5c0f43a5a5291488871df1b0c4630ea3ca2e
cobrelli <cobrelli@gmail.com>
2013-03-23 12:25:22 +0200

    varaus sivun rakentelua

--- PalveluBisnes/varaus.php
+++ PalveluBisnes/varaus.php
@@ -8,12 +8,55 @@ include 'sivunYlaOsa.php';
 
 <article id ="main">
     <div id="varaus_vasen">
-        <p>Tee uusi varaus</p>
+        <h3>Tee uusi varaus</h3>
+
+        <fieldset>
+            <p><label class="kyltti" for="etunimi">Etunimi: </label>
+                <input type="text" name="Etunimi" class="kentta">
+            </p>
+            <p><label class="kyltti" for="Sukunimi">Sukunimi: </label>
+                <input type="text" name="Sukunimi" class="kentta">
+            </p>
+            <p><label class="kyltti" for="asiakaspalvelija">Asiakaspalvelija: </label></p>
+            <select>
+                <option value="1">palvelija 1</options>
+                <option value="2">palvelija 2</options>
+                <option value="3">palvelija 3</options>
+            </select>
+            <p><label class="kyltti" for="toimipiste">Toimipiste: </label></p>
+            <select>
+                <option value="1">paikka 1</options>
+                <option value="2">paikka 2</options>
+                <option value="3">paikka 3</options>
+            </select>
+
+            <p><label class="kyltti" for="palvelu">Palvelu: </label></p> 
+            <select>
+                <option value="1">palvelu 1</options>
+                <option value="2">palvelu 2</options>
+                <option value="3">palvelu 3</options>
+            </select>
+
+            <p><label class="kyltti" for="pvm">Päivämäärä: </label>
+                <input type="date" name="pvm">
+            </p>
+            klo
+            <select>
+                <option value="8">8.00</options>
+                <option value="9">9.00</options>
+            </select>
+
+        </fieldset>
+        <input type="submit" value="Lähetä">
     </div>
+
     <div id="varaus_oikea">
-        <p>Peru varauksesi</p>
+        <h3>Peru varauksesi</h3>
+
+        Varausnumero: <input type="text" name="varausnumero"><br>
+        <input type="submit" value="Peru">
     </div>
-    
+
 </article>
 
 <?php
